6d2a4f19943125a844132dfacd202f0cdd1cd3ba
yladev <yanlinnaung881@gmail.com>
2023-01-29 11:29:58 +0630

    update-notification section

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ let navbar = document.querySelector('.menu');
 menu.onclick = () => {
     navbar.classList.toggle('active');
     menu.classList.toggle('move');
+    bell.classList.remove('active');
+}
+
+// Notification
+let bell = document.querySelector('.notification');
+
+document.querySelector('#bell-icon').onclick = () => {
+    bell.classList.toggle('active');
 }
