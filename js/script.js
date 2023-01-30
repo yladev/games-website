b38273246fa5529e7d28901f80b27b77d3e08f3f
yladev <yanlinnaung881@gmail.com>
2023-01-30 23:39:36 +0630

    code refactor

--- js/script.js
+++ js/script.js
@@ -13,3 +13,38 @@ let bell = document.querySelector('.notification');
 document.querySelector('#bell-icon').onclick = () => {
     bell.classList.toggle('active');
 }
+
+
+
+// Swipper
+var swiper = new Swiper(".trending-content", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  grabCursor: true,
+  loop: true,
+
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: true
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    1068: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
