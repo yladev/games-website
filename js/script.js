ae4150f923895f6d5d83990217e4a73b3f5bb6ad
yladev <yanlinnaung881@gmail.com>
2023-02-01 18:18:15 +0630

    complete-feature/custom-progress-bar

--- js/script.js
+++ js/script.js
@@ -47,3 +47,13 @@ var swiper = new Swiper(".trending-content", {
     },
   },
 });
+
+// Custom Scroll Bar
+window.onscroll = function() {mufunction()};
+
+function mufunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById('scroll-bar').style.width = scrolled + '%';
+}
